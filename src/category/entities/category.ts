6bbcd4b60038c629bb56aa55b2ab09e3717be477
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-09-23 00:44:51 -0300

    refactor: update the name of all properties

--- src/category/entities/category.ts
+++ src/category/entities/category.ts
@@ -1,57 +1,57 @@
 export class Category {
-  private _companyId: string;
-  private _name: string;
-  private _color: string;
+  private companyId: string;
+  private name: string;
+  private color: string;
 
-  private _categoryId: string;
-  private _createdAt: string;
-  private _updatedAt: string;
+  private categoryId: string;
+  private createdAt: string;
+  private updatedAt: string;
 
   constructor(companyId: string, name: string, color: string) {
-    this._companyId = companyId;
-    this._name = name;
-    this._color = color;
+    this.companyId = companyId;
+    this.name = name;
+    this.color = color;
 
     this.validate();
   }
 
   validate(): boolean {
-    if (this._companyId.length === 0) {
+    if (this.companyId.length === 0) {
       throw new Error('companyId is required');
     }
 
-    if (this._name.length === 0) {
+    if (this.name.length === 0) {
       throw new Error('name is required');
     }
 
-    if (this._color.length === 0) {
+    if (this.color.length === 0) {
       throw new Error('color is required');
     }
 
     return true;
   }
 
-  get companyId(): string {
-    return this._companyId;
+  get getCompanyId(): string {
+    return this.companyId;
   }
 
-  get name(): string {
-    return this._name;
+  get getName(): string {
+    return this.name;
   }
 
-  get color(): string {
-    return this._color;
+  get getColor(): string {
+    return this.color;
   }
 
-  set categoryId(value: string) {
-    this._categoryId = value;
+  set setCategoryId(value: string) {
+    this.categoryId = value;
   }
 
-  set createdAt(value: string) {
-    this._createdAt = value;
+  set setCreatedAt(value: string) {
+    this.createdAt = value;
   }
 
-  set updatedAt(value: string) {
-    this._updatedAt = value;
+  set setUpdatedAt(value: string) {
+    this.updatedAt = value;
   }
 }
