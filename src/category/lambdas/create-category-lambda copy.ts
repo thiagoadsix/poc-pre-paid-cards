4abcab86f71f79f6004a168630b8ea117cdd792d
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-09-23 00:42:23 -0300

    feat: add findAll lambda

---
+++ src/category/lambdas/create-category-lambda copy.ts
@@ -0,0 +1,48 @@
+import { HttpStatus } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import { validateOrReject } from 'class-validator';
+import { HandlerInterface } from '../../common/aws/handler/handler.interface';
+import { CategoryModule } from '../category.module';
+import { CategoryService } from '../services/category.service';
+import { HandlerResponse } from '../../common/aws/handler/handler.response';
+import { ExceptionFilter } from '../../common/filters/exception.filter';
+import { FindAllCategoryRequest } from './requests/find-all-category.request';
+
+export class FindAllCategoryLambda implements HandlerInterface {
+  async handler(event: APIGatewayEvent, context: Context): Promise<any> {
+    console.log('Event', JSON.stringify(event));
+
+    try {
+      console.log(`Stating lambda: ${context.functionName}`);
+
+      const appContext = await NestFactory.createApplicationContext(
+        CategoryModule,
+      );
+
+      const appService = appContext.get(CategoryService);
+
+      const request = new FindAllCategoryRequest({
+        companyId: event.requestContext.authorizer.claims['custom:companyId'],
+      });
+
+      try {
+        await validateOrReject(request);
+      } catch (errors) {
+        return ExceptionFilter.throw(errors);
+      }
+
+      try {
+        const resp = await appService.findAll(request.companyId);
+
+        console.log(`Finishing lambda: ${context.functionName}`);
+
+        return HandlerResponse.getResponse(resp, HttpStatus.OK);
+      } catch (error) {
+        return ExceptionFilter.throw(error);
+      }
+    } catch (error) {
+      return ExceptionFilter.throw(error);
+    }
+  }
+}
