f175db5ecfee0656cdb51e79c196288b6ef39478
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-09-23 00:44:29 -0300

    feat: create find all category request

---
+++ src/category/lambdas/requests/find-all-category.request.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
+export class FindAllCategoryRequest {
+  @IsNotEmpty()
+  @IsUUID()
+  companyId: string;
+
+  constructor(obj: Partial<FindAllCategoryRequest>) {
+    Object.assign(this, obj);
+  }
+}
