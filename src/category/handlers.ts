bcb7f79020f2982f372170b9c79c434051933e87
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-09-24 12:40:01 -0300

    refactor: update the name of the file

--- src/category/handlers.ts
+++ src/category/handlers.ts
@@ -1,5 +1,5 @@
 import { CreateCategoryLambda } from './lambdas/create-category-lambda';
-import { FindAllCategoryLambda } from './lambdas/create-category-lambda copy';
+import { FindAllCategoryLambda } from './lambdas/find-all-category-lambda';
 
 export const create = new CreateCategoryLambda().handler;
 export const findAll = new FindAllCategoryLambda().handler;
