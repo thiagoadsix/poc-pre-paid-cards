04d4a2a3ad8000857b9dcff818f649d830317eba
Thiago Andrade Silva <thiago.adsix@gmail.com>
2022-09-24 12:39:15 -0300

    fix: add src to includes and coverage to exclude

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
             path.resolve(__dirname, 'node_modules'),
             path.resolve(__dirname, '.serverless'),
             path.resolve(__dirname, '.webpack'),
+            path.resolve(__dirname, 'coverage'),
           ],
         ],
         options: {
